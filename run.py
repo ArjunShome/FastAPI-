31b059288bff51a59505897f6b2c6d00e7ac2310
Arjun Shome <arjunshome111@gmail.com>
2023-04-21 03:10:27 +0530

    Added base Pydentic Response Model concept

--- run.py
+++ run.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
+from enum import Enum
 
-app = FastAPI()
+import uvicorn
+from models import User
+
+my_app = FastAPI()
+
+
+class Nameenum(str, Enum):
+    Ajoy = "Ajoy"
+    Rama = "Rama"
+    Ayan = "Ayan"
+    Manjusa = "Manjusa"
+    Arjun = "Arjun"
+    Sanchita = "Sanchita"
 
 
 # Health check API
-@app.get("/")
+@my_app.get("/")
 def health_check():
     """_summary_
         Health check API to check run.
@@ -15,7 +28,7 @@ def health_check():
 
 
 # Path Pass and Parse API example
-@app.get("/Item/{item}")
+@my_app.get("/Item/{item}")
 def get_user_item(item: int):
     """_summary_
         Check path parse value
@@ -25,5 +38,34 @@ def get_user_item(item: int):
     Returns:
         json:The item number
     """
-    return {"item":item}
+    return {"item": item}
 
+
+# Adding Enums to path API example
+@my_app.get("/Names/{user_name}")
+def get_user_name(user_name: Nameenum):
+    if user_name is Nameenum.Ajoy:
+        return {"Name": f"{user_name}", "message": "The Father in the house"}
+    if user_name is Nameenum.Rama:
+        return {"Name": f"{user_name}", "message": "The Mother in the house"}
+    if user_name is Nameenum.Ayan:
+        return {"Name": f"{user_name}", "message": "The elder son of the house"}
+    if user_name is Nameenum.Manjusa:
+        return {"Name": f"{user_name}", "message": "The wife of elder son in the house"}
+    if user_name is Nameenum.Arjun:
+        return {"Name": f"{user_name}", "message": "The younger son of the house"}
+    if user_name is Nameenum.Sanchita:
+        return {"Name": f"{user_name}", "message": "The wife of younder son in the house"}
+
+
+# Pydentic Model
+@my_app.post("/User/Create")
+def create_user(user: User):
+    incoming_user = user.dict()
+    message = "Aadhar and Pan Mandatory" if user.is_adult else "User is a child"
+    incoming_user.update({"usr_msg": message})
+    return incoming_user
+
+
+if __name__ == "__main__":
+    uvicorn.run("run:my_app", reload=True) #type: ignore
